afc4bab6c187547b24f2f9e178b72090f506ff54
kinamura <i.kosuke0801@gmail.com>
2025-09-27 14:19:08 +0900

    migration

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -3,3 +3,6 @@ CREATE UNIQUE INDEX idx_users_user_name ON `users` (`user_name`);
 
 CREATE INDEX idx_orders_user_id_created_at ON orders (user_id, created_at);
 CREATE INDEX idx_orders_shipped_status_product ON orders (shipped_status, product_id);
+
+CREATE INDEX idx_products_name ON products (name);
+CREATE FULLTEXT INDEX idx_products_description ON products (description);
